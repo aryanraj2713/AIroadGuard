d06eb868750296df82c8bfaa5774ef32bb63aa4c
Mohd Zaid <zaid.tab123@gmail.com>
2022-09-03 14:16:40 +0530

    ✨ add accident to db

--- be/src/api/accident/controller.ts
+++ be/src/api/accident/controller.ts
@@ -1,5 +1,24 @@
 import ErrorClass from "../../helper/types/error";
 import database from "../../loaders/database";
 
-export const accident = async () => {};
-
+export const accident = async (cameraID:string, accidentImage: string) => {
+  try {
+    const data = await (await database())
+      .collection("accidents")
+      .insertOne({accidentImage:accidentImage,cameraID:cameraID});
+    if (!data) {
+      throw new ErrorClass("Error uploading image", 400);
+    }
+    console.log(`${data.insertedCount} food data added`);
+    return {
+      success: true,
+      status: 201,
+      message: 'Accident image uploaded',
+    };
+  } catch (error) {
+    throw new ErrorClass(
+      error.message ?? "Image updation failed",
+      error.status.code ?? 500
+    );
+  }
+};
--- be/src/api/accident/router.ts
+++ be/src/api/accident/router.ts
@@ -4,11 +4,16 @@ import { accident } from "./controller";
 
 const accidentRouter = Router();
 
-accidentRouter.post("/", handleAccident);
+accidentRouter.post("/add", handleAccident);
 
 async function handleAccident(req: Request, res: Response, next: NextFunction) {
   try {
-    const result = await accident();
+    const result = await accident(req.body.cameraID, req.body.imageArray);
+    if (result.success) {
+      res
+        .status(result.status)
+        .json({ success: true, message: result.message });
+    }
   } catch (error) {
     LoggerInstance.error(error);
     next(error);
