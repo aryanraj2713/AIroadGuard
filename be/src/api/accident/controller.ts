5f8e9ab7f09915e14b4fc885232ce52269684157
Mohd Zaid <zaid.tab123@gmail.com>
2022-09-03 15:14:50 +0530

    ✨ add time

--- be/src/api/accident/controller.ts
+++ be/src/api/accident/controller.ts
@@ -1,11 +1,24 @@
 import ErrorClass from "../../helper/types/error";
 import database from "../../loaders/database";
 
-export const accident = async (cameraID:string, accidentImage: string) => {
+export const accident = async (cameraID: string, accidentImage: string) => {
   try {
-    const data = await (await database())
-      .collection("accidents")
-      .insertOne({accidentImage:accidentImage,cameraID:cameraID});
+    const data = await (await database()).collection("accidents").insertOne({
+      accidentImage: accidentImage,
+      cameraID: cameraID,
+      time:
+        new Date().getDate() +
+        "/" +
+        (new Date().getMonth() + 1) +
+        "/" +
+        new Date().getFullYear() +
+        " " +
+        new Date().getHours() +
+        ":" +
+        new Date().getMinutes() +
+        ":" +
+        new Date().getSeconds(),
+    });
     if (!data) {
       throw new ErrorClass("Error uploading image", 400);
     }
@@ -13,9 +26,10 @@ export const accident = async (cameraID:string, accidentImage: string) => {
     return {
       success: true,
       status: 201,
-      message: 'Accident image uploaded',
+      message: "Accident image uploaded",
     };
   } catch (error) {
+    console.log(error);
     throw new ErrorClass(
       error.message ?? "Image updation failed",
       error.status.code ?? 500
