1980bc7de2e757546a859591fff27f2bc183513b
Mohd Zaid <zaid.tab123@gmail.com>
2022-09-03 11:43:58 +0530

    ✨ add error handler

---
+++ be/src/helper/types/error.ts
@@ -0,0 +1,9 @@
+class ErrorClass extends Error {
+  statusCode: number;
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
+export default ErrorClass;
