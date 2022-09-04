62f01791337efe9881c167f66f231b8f700f6a57
Srivastava57Harsh <srivastava57harsh@gmail.com>
2022-09-04 09:46:01 +0530

    :bug: Fix Dynamixc data
    Fix Dynamixc data

--- client/components/Table/TableContainer.js
+++ client/components/Table/TableContainer.js
@@ -7,7 +7,6 @@ import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -18,7 +17,6 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
     fontSize: 14,
   },
 }));
-
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
   "&:nth-of-type(odd)": {
     backgroundColor: theme.palette.action.hover,
@@ -29,45 +27,27 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
-function createData(location, time, camid) {
-  return { location, time, camid };
+function createData(location, time, flag, camid) {
+  return { location, time, flag, camid };
 }
 
-const data = await axios.get("http://localhost:8080/api/accident/getAccident", {
-  headers: { cameraID: "ENH45" },
-});
-
-function getRandom(min, max) {
-  return Math.random() * (max - min) + min;
-}
-
-function sleep() {
-  ms = getRandom(2, 10) * 1000;
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-async function renderAccidents(accData) {
-  for (let i = 0; i < length(accData); i++) {
-    <StyledTableRow key={accDataaccData[i].cameraID}>
-      <StyledTableCell component="th" scope="row">
-        {accData[i].location}
-      </StyledTableCell>
-      <StyledTableCell align="right">{accData[i].time}</StyledTableCell>
-      <StyledTableCell align="right">{accData[i].camid}</StyledTableCell>
-    </StyledTableRow>;
-    await sleep();
-  }
-}
 const rows = [
-  createData("Potheri", "08:56", "E9H45"),
-  createData("Guduvenchary", "08:56", "E9H64"),
-  createData("Potheri", "09:02", "E9H34"),
-  createData("Potheri", "09:02", "E9H68"),
-  createData("Maraimalai Nagar", "09:08", "E9H67"),
-  createData("KattanKulathur", "09:09", "E9H90"),
+  createData("Potheri", "08:56", "true", "E9H45"),
+  createData("Guduvenchary", "08:56", "true", "E9H64"),
+  createData("Potheri", "09:02", "true", "E9H34"),
+  createData("Potheri", "09:02", "true", "E9H68"),
+  createData("Maraimalai Nagar", "09:08", "true", "E9H67"),
+  createData("KattanKulathur", "09:09", "true", "E9H90"),
 ];
 
 export default function CustomizedTables() {
+  //   const data = await axios.get(
+  //     "http://localhost:8080/api/accident/getAccident",
+  //     {
+  //       headers: { cameraID: "ENH45" },
+  //     }
+  //   );
+
   return (
     <TableContainer>
       <Table sx={{ minWidth: 500 }} aria-label="customized table">
@@ -75,11 +55,109 @@ export default function CustomizedTables() {
           <TableRow>
             <StyledTableCell>Location</StyledTableCell>
             <StyledTableCell align="right">Time</StyledTableCell>
-            <StyledTableCell align="right">Camera</StyledTableCell>
+            <StyledTableCell align="right">isAccident</StyledTableCell>
+            <StyledTableCell>Camera Id</StyledTableCell>
           </TableRow>
         </TableHead>
-        <TableBody>{renderAccidents(data)}</TableBody>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.camid}>
+              <StyledTableCell component="th" scope="row">
+                {row.location}
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.time}</StyledTableCell>
+              <StyledTableCell align="right">{row.flag}</StyledTableCell>
+              <StyledTableCell align="right">{row.camid}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
       </Table>
     </TableContainer>
   );
 }
+
+// import * as React from "react";
+// import axios from "axios";
+// import { styled } from "@mui/material/styles";
+// import Table from "@mui/material/Table";
+// import TableBody from "@mui/material/TableBody";
+// import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+// import TableContainer from "@mui/material/TableContainer";
+// import TableHead from "@mui/material/TableHead";
+// import TableRow from "@mui/material/TableRow";
+// import Paper from "@mui/material/Paper";
+
+// const StyledTableCell = styled(TableCell)(({ theme }) => ({
+//   [`&.${tableCellClasses.head}`]: {
+//     backgroundColor: theme.palette.common.black,
+//     color: theme.palette.common.white,
+//   },
+//   [`&.${tableCellClasses.body}`]: {
+//     fontSize: 14,
+//   },
+// }));
+
+// const StyledTableRow = styled(TableRow)(({ theme }) => ({
+//   "&:nth-of-type(odd)": {
+//     backgroundColor: theme.palette.action.hover,
+//   },
+//   // hide last border
+//   "&:last-child td, &:last-child th": {
+//     border: 0,
+//   },
+// }));
+
+// function createData(location, time, camid) {
+//   return { location, time, camid };
+// }
+
+// const data = await axios.get("http://localhost:8080/api/accident/getAccident", {
+//   headers: { cameraID: "ENH45" },
+// });
+
+// function getRandom(min, max) {
+//   return Math.random() * (max - min) + min;
+// }
+
+// function sleep() {
+//   ms = getRandom(2, 10) * 1000;
+//   return new Promise((resolve) => setTimeout(resolve, ms));
+// }
+
+// async function renderAccidents(accData) {
+//   for (let i = 0; i < length(accData); i++) {
+//     <StyledTableRow key={accDataaccData[i].cameraID}>
+//       <StyledTableCell component="th" scope="row">
+//         {accData[i].location}
+//       </StyledTableCell>
+//       <StyledTableCell align="right">{accData[i].time}</StyledTableCell>
+//       <StyledTableCell align="right">{accData[i].camid}</StyledTableCell>
+//     </StyledTableRow>;
+//     await sleep();
+//   }
+// }
+// const rows = [
+//   createData("Potheri", "08:56", "E9H45"),
+//   createData("Guduvenchary", "08:56", "E9H64"),
+//   createData("Potheri", "09:02", "E9H34"),
+//   createData("Potheri", "09:02", "E9H68"),
+//   createData("Maraimalai Nagar", "09:08", "E9H67"),
+//   createData("KattanKulathur", "09:09", "E9H90"),
+// ];
+
+// export default function CustomizedTables() {
+//   return (
+//     <TableContainer>
+//       <Table sx={{ minWidth: 500 }} aria-label="customized table">
+//         <TableHead>
+//           <TableRow>
+//             <StyledTableCell>Location</StyledTableCell>
+//             <StyledTableCell align="right">Time</StyledTableCell>
+//             <StyledTableCell align="right">Camera</StyledTableCell>
+//           </TableRow>
+//         </TableHead>
+//         <TableBody>{renderAccidents(data)}</TableBody>
+//       </Table>
+//     </TableContainer>
+//   );
+// }
